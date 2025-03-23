cd5596c75e402ade590fa820b597ffc3c116e0f4
pranjul jangra <pranjuljan68@gmail.com>
2025-03-23 12:05:19 +0530

    added vercel.json file

--- src/layouts/Home.jsx
+++ src/layouts/Home.jsx
@@ -33,7 +33,6 @@ export default function Home() {
 
 
   const user = localStorage.getItem('proPlannerUsername');
-  const token = localStorage.getItem('proPlannerAccessToken');
 
   const fetchTasks = () => {  // it is declared outside of useEffect so that it can re-run when handleAddTask function will trigger
     
@@ -41,16 +40,12 @@ export default function Home() {
       notifyUser('User not found or the session has expired. Please login again.');
       localStorage.removeItem('proPlannerUsername');
       localStorage.removeItem('proPlannerAccessToken');
-
       return navigate('/');
     }
 
     setIsLoaderActive(true);
 
-    apiClient.get(`/home/data`, {
-        headers: { Authorization: `Bearer ${token}` },
-        params: { user },
-      })
+    apiClient.get(`/home/data`, { params: { user } })
       .then((response) => {
         const sortedTasks = response.data.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt));
         setDailyTasks(sortedTasks.filter((t) => t.countdownPeriod === 'daily'));
@@ -68,7 +63,7 @@ export default function Home() {
   
   useEffect(() => {
     fetchTasks();
-  }, [user]);
+  }, []);
   
 
   async function handleAddTask(taskText, countdownPeriod) {
@@ -141,27 +136,25 @@ export default function Home() {
 
       <section>
 
-        <div className={`homepage ${theme}`}>
-          <nav aria-label="Main navigation">
-            <Suspense fallback={<div>Loading...</div>}>
+        <Suspense fallback={<div>Loading...</div>}>
+          <div className={`homepage ${theme}`}>
+            <nav aria-label="Main navigation">
               <Navigations /> 
-            </Suspense>
-          </nav>
-          <div>
-            <h2 className="visually-hidden">Tasks</h2>
-            <AddTasks onAdd={handleAddTask} onEdit={handleUpdateTask} />
+            </nav>
             <div>
-              <Suspense fallback={<div>Loading...</div>}>
+              <h2 className="visually-hidden">Tasks</h2>
+              <AddTasks onAdd={handleAddTask} onEdit={handleUpdateTask} />
+              <div>
                 <Taskcontainer setIsModalOpen={setIsModalOpen} shouldCompleteButtonDisplay={true} handleCompleteTask={handleCompleteTask} deleteFn={handleDeleteTask} tasks={dailyTasks} containerName="📋 Daily Tasks" />
                 <Taskcontainer setIsModalOpen={setIsModalOpen} shouldCompleteButtonDisplay={true} handleCompleteTask={handleCompleteTask} deleteFn={handleDeleteTask} tasks={weeklyTasks} containerName="📆 Weekly Tasks" />
-              </Suspense>
+              </div>
+            </div>
+            <div>
+              <h2 className="visually-hidden">Notes</h2>
+              <Taskcontainer setIsModalOpen={setIsModalOpen} shouldCompleteButtonDisplay={false} deleteFn={handleDeleteTask} tasks={notes} containerName='🗒️ Notes' />
             </div>
           </div>
-          <div>
-            <h2 className="visually-hidden">Notes</h2>
-            <Taskcontainer setIsModalOpen={setIsModalOpen} shouldCompleteButtonDisplay={false} deleteFn={handleDeleteTask} tasks={notes} containerName='🗒️ Notes' />
-          </div>
-        </div>
+        </Suspense>
 
 
 
